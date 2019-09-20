50f5c715cbca7529955cc093008b3f70dcca932d
Fredrik Leemann <fredrik@leemann.se>
2019-09-21 00:19:36 +0200

    ...

--- app.js
+++ app.js
@@ -7,11 +7,70 @@ const data = audio.concat(video)
 
 app.get('/', (req, res) => {
     res.send(data)
-  })
+})
+  
+app.get('/audio/', (req, res) => {
+    res.send(audio)
+})
 
-app.get("/people", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
+app.get('/video/', (req, res) => {
+    res.send(video)
+})
+  
+app.get('/audio/:id', (req, res) => {
+    const { id } = req.params
+    const resource = audio.find(function(obj) {
+      return obj.id == id
+    })
+    if (resource) {
+      res.send(resource)
+    }
+    res.status(404).send('{}')
+})
+  
+app.get('/video/:id', (req, res) => {
+    const { id } = req.params
+    const resource = video.find(function(obj) {
+      return obj.id == id
+    })
+    if (resource) {
+      res.send(resource)
+    }
+    res.status(404).send('{}')
+})
+  
+app.get('/id/:id', (req, res) => {
+    const { id } = req.params
+    const resource = data.find(function(obj) {
+      return obj.id == id
+    })
+    if (resource) {
+      res.send(resource)
+    }
+    res.status(404).send('{}')
+})
+  
+app.get('/search/:query', (req, res) => {
+    const { query } = req.params
+    const objects = data.filter(function(obj) {
+      return obj.tags.indexOf(query) >= 0
+    })
+    if (objects) {
+      res.send(objects)
+    }
+    res.status(404).send('{}')
+})
+  
+app.get('/category/:category', (req, res) => {
+    const { category } = req.params
+    const objects = data.filter(function(obj) {
+      return obj.categories.indexOf(category) >= 0
+    })
+    if (objects) {
+      res.send(objects)
+    }
+    res.status(404).send('{}')
+})
 
 const PORT = process.env.NODE_ENV == 'production' ? process.env.PORT : 3000;
 const BOOT = `Express server started and listening on port: ${PORT}`;
